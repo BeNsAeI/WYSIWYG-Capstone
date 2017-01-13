1dfeb42be4f6ed4470ccfb8da67a661e24996b6a
Connor Sedwick <connorsedwick@10-249-16-151.wireless.oregonstate.edu>
2017-01-13 14:28:57 -0800

    Add channel

--- dist/Blocks/Channels.py
+++ dist/Blocks/Channels.py
@@ -2,3 +2,12 @@
 # This file describes the Channel GUI class
 # Objects of such class have the following attributes:
 # This file also contains the tests for the Block module
+class Channel:
+	def __init__(self,source,destination,id,w,h,x=0,y=0):
+		self.Source=source
+		self.Destination=destination
+		self.ID=id
+	def changeSource(self,source):
+		self.Source=source
+	def changeDestination(self,destination):
+		self.Destination=destination
