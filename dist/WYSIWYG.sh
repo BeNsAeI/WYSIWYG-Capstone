58ee7237d2666f61aeea058df109be240c96bbd2
BeNsAeI <behnam.saeidi@gmail.com>
2017-01-20 13:50:46 -0800

    Added the installer and executable

--- dist/WYSIWYG.sh
+++ dist/WYSIWYG.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 # This is the main execution sript
 python Main.py
+sudo python Interface/WYSIWYG-Prototype.py
---
+++ dist/install.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+echo "Installing WYSIWYG Tensorflow GUI"
+sudo apt-get install python
+sudo apt-get install python-pip
+sudo add-apt-repository ppa:kivy-team/kivy
+sudo apt-get install python-kivy
+sudo apt-get install python-kivy-examples
