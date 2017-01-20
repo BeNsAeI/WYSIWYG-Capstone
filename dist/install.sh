f1226d7347060860539906166d05b9081c7ca8ac
BeNsAeI <behnam.saeidi@gmail.com>
2017-01-20 14:19:21 -0800

    graphic package was added

--- dist/install.sh
+++ dist/install.sh
@@ -1,7 +1,19 @@
 #!/bin/sh
 echo "Installing WYSIWYG Tensorflow GUI"
 sudo apt-get install python
+echo "Done."
+echo "Python installed."
 sudo apt-get install python-pip
+echo "pip installed"
+echo "installing numPy"
+sudo pip install numpy
+echo "Done."
+echo "adding kivy to apt repo"
 sudo add-apt-repository ppa:kivy-team/kivy
+echo "installing kivy"
 sudo apt-get install python-kivy
+echo "Done."
+echo "installing kivy examples"
 sudo apt-get install python-kivy-examples
+clear
+echo "Done."
