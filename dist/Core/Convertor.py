05805913845958723d233b0f52dd027cf715823d
BeNsAeI <behnam.saeidi@gmail.com>
2017-02-02 08:23:49 -0800

    Added the Convertor.py as a core file

---
+++ dist/Core/Convertor.py
@@ -0,0 +1,14 @@
+# This core file has the task of turning the GUI into a machiene readable data
+# STeps  involved:
+# Create a priority table
+# follow the channels direction
+# add items to the list in order
+# save the data to a file
+# Formatting:
+#  __________ ____ _________
+# | Priority | ID | statues |
+#  ---------- ---- ---------
+# |     1    | 25 |  Valid  |
+#              .
+#              .
+#              .
