4037ed5e4dce11c183a39614707158392a1b60e9
DESKTOP-JJ9IVB6\Connor Sedwick <sedwickc@oregonstate.edu>
2017-02-06 19:35:51 -0800

    Adds functional dropdown for top menu options.

--- dist/Interface/WYSIWYG-Prototype.py
+++ dist/Interface/WYSIWYG-Prototype.py
@@ -3,11 +3,17 @@ kivy.require("1.9.0")
 
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.dropdown import DropDown
+from kivy.uix.button import Button
+from kivy.lang import Builder
+
+Builder.load_file('DocumentOptions.kv')
 
 class SampGridLayout(GridLayout):
     pass
 
 class WYSIWYGApp(App):
+
     def build(self):
         return SampGridLayout()
 
