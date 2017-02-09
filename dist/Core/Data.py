1ea82384f0a4c7a7c63b048147887c40fadee145
BeNsAeI <behnam.saeidi@gmail.com>
2017-02-09 08:37:32 -0800

    Data handling

--- dist/Core/Data.py
+++ dist/Core/Data.py
@@ -1,12 +1,28 @@
 # This file contains a module for connecting to database
 # it could be included included in differrent core files for data reading and book keeping purposes
 # mySQL and This also generates local files
-
+import MySQLdb
 class DataPy:
-	def __init__(self):
-	def DB():
-	def Local():
+	def DB(op):
+		import MySQLdb
+		#Write
+		if (op):
+			
+		#read
+		else:
+			
+
+	def Local(op):
+		#write
+		if (op):
+
+		#read
+		else:
+
 	def Write():
+
 	def Read();
+
 def testCase():
+
 testCase()
