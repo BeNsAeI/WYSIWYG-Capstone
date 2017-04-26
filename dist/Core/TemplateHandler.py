8904bfa2493cc9c8869cb8bf6c0a839e095d0777
BeNsAeI <behnam.saeidi@gmail.com>
2017-04-26 12:32:50 -0700

    fixed the read file

--- dist/Core/TemplateHandler.py
+++ dist/Core/TemplateHandler.py
@@ -4,18 +4,20 @@ class fileIO:
 	def __init__(self, fileType):
 		self.fileType = fileType;
 		self.extractedTempCode = '';
-		self.pathName = '/TestFlder';
+		self.pathName = '../Templates';
+		self.extention = '.template.py';
 
-	def read_fil(self):
-		with open(self.fileType+'.template', 'r') as myfile:
-			data = myfile.read().replace('\n', '__/')
+	def read_fil(self,name):
+		data = ''
+		with open(self.pathName+"/"+name+self.extention, 'r') as myfile:
+			data =data+ myfile.read().replace('\n', '\n')
 		return data;
 
 	def writefil(self):
-		with open(self.fileType+'.template', 'r') as myfile:
-			myfile.write().replace('__/', '\n')
+		with open(self.pathName+"/"+name+self.extention, 'w') as myfile:
+			myfile.write().replace('\n', '\n')
 		return data;
 
 if __name__ == '__main__':
     temp = fileIO("testCase");
-    print(temp.read_fil());
+    print(temp.read_fil("for"));
