6c787220b5e1bb7468851ea6fba11223dd63a56f
BeNsAeI <behnam.saeidi@gmail.com>
2017-04-28 10:23:01 -0700

    added the debugger

---
+++ dist/Debugger/errorHandler.py
@@ -0,0 +1,10 @@
+# This function grabs the error, output and string from command line while the code is running
+class errorHandler:
+	def __init__(self):
+		pass
+	def Monitor(self):
+		pass
+	def __makeReport(self):
+		pass
+	def logToFile(self):
+		pass
