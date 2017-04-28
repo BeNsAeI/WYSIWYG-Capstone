db9cf778f46d260a039c5f7a50354a01c80a0105
BeNsAeI <behnam.saeidi@gmail.com>
2017-04-28 10:52:59 -0700

    added the error handler to turn python shell to string

--- dist/Debugger/errorHandler.py
+++ dist/Debugger/errorHandler.py
@@ -1,13 +1,41 @@
 # This function grabs the error, output and string from command line while the code is running
+import subprocess
+import sys
+'''def run(*popenargs, input=none, check = False, **kwargs):
+	if input is not None:
+		if 'stdin' in kwargs:
+			raise ValueError('stdin and input arguments may not both be used.')
+		kwargs['stdin'] = subprocess.PIPE
+
+	process = subprocess.Popen(*popenargs, **kwargs):
+	try:
+		stdout, stderr = process.communicate(input)
+	except:
+		process.kill()
+		process.wait()
+		raise
+	retcode = process.poll()
+	if check and retcode:
+		raise subprocess.CalledProcessError(
+			retcode, process.args, output=stdout, stderr=stderr)
+	return retcode, stdout, stderr
+'''
 class errorHandler:
-	def __init__(self):
-		pass
+	def __init__(self,o,f=''):
+		self.command = "python ./"+o
+		self.flag = f
+		result = "No results"
 	def Monitor(self):
-		pass
-	def __makeReport(self):
-		pass
+		print("Command: "+self.command+" "+self.flag)
+		p = subprocess.Popen([self.command, self.flag], stdout=subprocess.PIPE,stderr=subprocess.PIPE,shell=True)
+		self.result, err = p.communicate()
+	def makeReport(self):
+		print("Print: ")
+		print(self.result)
 	def logToFile(self):
 		pass
-def testcase:
-	pass
+def testcase():
+	temp = errorHandler("Output.py")
+	temp.Monitor()
+	temp.makeReport()
 testcase()
