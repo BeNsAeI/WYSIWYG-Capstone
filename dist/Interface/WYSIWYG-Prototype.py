648d4fe53d0c003a4280c47f388c7bb24f99bf99
BeNsAeI <behnam.saeidi@gmail.com>
2017-04-30 13:53:32 -0700

    error with type

--- dist/Interface/WYSIWYG-Prototype.py
+++ dist/Interface/WYSIWYG-Prototype.py
@@ -307,7 +307,7 @@ class BuilderSuite(BoxLayout):
 		temp = Generator()
 		II = 0
 		for i in blocks:
-			if is_number(i.Value)==false:
+			if is_number(i.Value)==False:
 				i.Value = '"'+i.Value+'"'
 			print(i.Name+", "+i.Type+", "+i.Caption+", "+str(i.ID)+".")
 			comment = "Code for "+i.Name+" block; "+i.Caption+":"
