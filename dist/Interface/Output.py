20f492198f34933809f7caf7d31fd3bc37f9451c
Collin Dorsett <dorsettc@oregonstate.edu>
2017-05-03 13:05:24 -0700

    Resize Channel Button

--- dist/Interface/Output.py
+++ dist/Interface/Output.py
@@ -1,6 +1,11 @@
 # Generate with Tensorflow WYSIWYG GUI
-'''Code for test block; Add Caption:'''
-test=1
-'''Code for output0 block; Add Caption:'''
-print(test);
+'''This is a variable declearation'''
+string="Hello world!"
+'''This is a variable declearation'''
+x=10
+'''This is a comment made by Ben'''
+for i in range(0,x):
+	
+	'''testing print'''
+	print(i);
 
