71f924e12d3b0c5a92b9138fcc7401ef1125d25e
Collin Dorsett <dorsettc@oregonstate.edu>
2017-05-03 12:44:30 -0700

    Checkbox to Button

--- dist/Interface/Output.py
+++ dist/Interface/Output.py
@@ -1,11 +1,6 @@
 # Generate with Tensorflow WYSIWYG GUI
-'''This is a variable declearation'''
-string="Hello world!"
-'''This is a variable declearation'''
-x=10
-'''This is a comment made by Ben'''
-for i in range(0,x):
-	
-	'''testing print'''
-	print(i);
+'''Code for test block; Add Caption:'''
+test=1
+'''Code for output0 block; Add Caption:'''
+print(test);
 
