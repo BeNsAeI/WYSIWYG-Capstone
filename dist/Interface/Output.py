efaadf6d64b69e69c59ebb9280c91cca31bf2728
BeNsAeI <behnam.saeidi@gmail.com>
2017-05-15 12:35:04 -0700

    added all files

--- dist/Interface/Output.py
+++ dist/Interface/Output.py
@@ -1,10 +1,11 @@
 # Generate with Tensorflow WYSIWYG GUI
-'''Code for test block; Add Caption:'''
-test=13+4
-'''Code for test2 block; Add Caption:'''
-test2=test
-'''Code for test3 block; Add Caption:'''
-test3=test2-test
-'''Code for output0 block; Add Caption:'''
-print(test3);
+'''This is a variable declearation'''
+string="Hello world!"
+'''This is a variable declearation'''
+x=10
+'''This is a comment made by Ben'''
+for i in range(0,x):
+	
+	'''testing print'''
+	print(i);
 
