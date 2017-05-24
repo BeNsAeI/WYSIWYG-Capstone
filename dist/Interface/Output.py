04a7750525eb0bdad0b704e856e31182a57a2af9
BeNsAeI <behnam.saeidi@gmail.com>
2017-05-24 12:57:30 -0700

    fixed bugs

--- dist/Interface/Output.py
+++ dist/Interface/Output.py
@@ -2,8 +2,22 @@
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
-'''Code for tf.acos0 block; Add Caption:, Name could be None'''
-tf_acos0=tf.acos(<<ARG1>>)
-'''Code for variable0 block; Add Caption:'''
-variable0=tf_acos0
+'''Code for a block; Add Caption:'''
+a=[[1],[1]]
+'''Code for b block; Add Caption:'''
+b=[[1],[1]]
+'''Code for variable3 block; Add Caption:'''
+variable3=a
+'''Code for variable4 block; Add Caption:'''
+variable4=b
+'''Code for output1 block; Add Caption:'''
+print(variable3);
+'''Code for output2 block; Add Caption:'''
+print(variable4);
+'''Code for tf.add0 block; Add Caption:, Name could be None'''
+tf_add0=tf.add([[1],[1]],[[1],[1]] )
+'''Code for c block; Add Caption:'''
+c=tf_add0
+'''Code for output0 block; Add Caption:'''
+print(c);
 
