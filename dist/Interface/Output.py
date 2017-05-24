8fa7b4dec384dfb7d1cc28ba940fa2998e2f5b06
Collin Dorsett <dorsettc@oregonstate.edu>
2017-05-24 13:35:59 -0700

    Probe Block Layout

--- dist/Interface/Output.py
+++ dist/Interface/Output.py
@@ -1,23 +1,10 @@
-# Generate with Tensorflow WYSIWYG GUI
-import tensorflow as tf
-import numpy as np
-import matplotlib.pyplot as plt
-'''Code for a block; Add Caption:'''
-a=[[1],[1]]
-'''Code for b block; Add Caption:'''
-b=[[1],[1]]
-'''Code for variable3 block; Add Caption:'''
-variable3=a
-'''Code for variable4 block; Add Caption:'''
-variable4=b
-'''Code for output1 block; Add Caption:'''
-print(variable3);
-'''Code for output2 block; Add Caption:'''
-print(variable4);
-'''Code for tf.add0 block; Add Caption:, Name could be None'''
-tf_add0=tf.add([[1],[1]],[[1],[1]] )
-'''Code for c block; Add Caption:'''
-c=tf_add0
-'''Code for output0 block; Add Caption:'''
-print(c);
+'''This is a variable declearation'''
+"Hello world!"=<<ARG2>>
+'''This is a variable declearation'''
+10=<<ARG2>>
+'''This is a comment made by Ben'''
+for range(0,x) in :
+<<ARG3>>
+	'''testing print'''
+	print(<<ARG1>>);
 
